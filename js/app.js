4b9793af51dfa2da00157c6276080041eae17ee9
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-04 11:41:44 -0500

    create render message for state of game

--- js/app.js
+++ js/app.js
@@ -14,11 +14,11 @@ let winner
 	  // or a game that is still in play.
 /*------------------------ Cached Element References ------------------------*/
 
-    const squares = document.querySelectorAll('.square')
-    console.log(squares)
+  let squares = document.querySelectorAll('.square')
+  // refers to each element with a class of square= all of the (divs) squares
 
 	// 2.2) Store the element that displays the game status on the page.
-  const gameStatus = document.getElementById('message')
+  let gameStatus = document.getElementById('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -43,6 +43,7 @@ let winner
 
    function render() {
     for(let i = 0; i < boardArray.length; i++){
+      // the squares will change color depending on their value (depending on if they're occupied by player 1, 2 or not occupied)
       let currentSquare = boardArray[i]
       if(currentSquare === -1){
         squares[i].style.backgroundColor = 'green'
@@ -51,15 +52,17 @@ let winner
       } else{
         squares[i].style.backgroundColor= 'red'
       }
-      
     }
-  
+    if(winner !== null){
+      gameStatus.textContent = `It's player ${turn}'s turn!` 
+    } else if(winner === "T") {
+      gameStatus.textContent = "It's a tie!"
+    } else{
+      gameStatus.textContent = `Congratulations! Player ${turn} won!`
+    }
   } 
   
-  // 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:
-    // 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
-
-    // 3.3.1.2) Style that square however you wish dependant on the value contained in the current cell being iterated over (-1, 1, or null)
+  
   // 3.3.2) Render a message reflecting the currrent game state:
     // 3.3.2.1) If winner has a value other than null (game still in progress), render whose turn it is.
       // Hint: Maybe use a ternary inside of a template literal here?
