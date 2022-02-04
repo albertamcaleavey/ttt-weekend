b5905c9d8e33088fa94d2226eb48dd649fb2246b
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-03 19:15:52 -0500

    create init function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let squaresOnBoard = []
+let boardArray = []
 // 1.1) Use an array to represent the squares on the board.
 let turn
 // 1.2) Use a turn variable to track whose turn it is.
@@ -15,9 +15,9 @@ let winner
 /*------------------------ Cached Element References ------------------------*/
 	// 2.1) Store the 9 elements that represent the squares on the page.
 	  // You may want to give each square a class name in your HTML to make this easier!
-    const square = document.querySelectorAll('.square')
+    const squares = document.querySelectorAll('.square')
      // should I select them by class or element?
-// console.log(square)
+    console.log(squares)
 	// 2.2) Store the element that displays the game status on the page.
   const gameStatus = document.getElementById('message')
 
@@ -27,18 +27,17 @@ let winner
 
 
 /*-------------------------------- Functions --------------------------------*/
+  // INITIALIZE FUNCTION
 
-// 3.2) That initialize function should initialize the state variables:
-	  // 3.2.1) Initialize the board array to 9 nulls to represent empty squares. 
-	    // The 9 elements will "map" to each square.
-	    // Index 0 represents the top-left square.
-	    // Index 1 represents the top-middle square.
-			// So on, continuing through the entire board until...
-	    // Index 8 maps to the bottom-right square.
-	  // 3.2.2) Initialize whose turn it is to 1 (player 'X'). 
-	    // Player 'O' will be represented by -1.
-	  // 3.2.3) Initialize the winner variable to null.
-	    // This represents that there is no winner or tie yet. 
-	    // The winner variable will hold the player value (1 or -1) if there's a winner. 
-	    // The winner will hold a 'T' if there's a tie.
-	  // 3.2.4) Render those state variables to the page by calling a render function.
+    init()
+
+    function init(){
+      boardArray = [null, null, null, null, null, null, null, null, null]
+
+      console.log(boardArray)
+      turn = 1
+      winner = null
+      render()
+    }
+
+   
