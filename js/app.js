9e0956c9a16be538e02df38c1141c4b0a0e72190
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-06 15:24:24 -0500

    update notes and remove console logs

--- js/app.js
+++ js/app.js
@@ -18,11 +18,9 @@ let winner
 let winnerName
 
 /*------------------------ Cached Element References ------------------------*/
-  let squares = document.querySelectorAll('.square')
+let squares = document.querySelectorAll('.square')
 
-  let gameStatus = document.getElementById('message')
-
- 
+let gameStatus = document.getElementById('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -46,7 +44,7 @@ document.getElementById('replay').addEventListener('click', init, render)
    // RENDER FUNCTION
    function render() {
     for(let i = 0; i < boardArray.length; i++){
-      // the squares will change depending on their value (depending on if they're occupied by player 1, 2 or not occupied)
+      // displays an X or O on the spot on the board that corresponds to the current index, depending on the value of that index
       if(boardArray[i] === -1){
         squares[i].textContent = 'O'
       } else if(boardArray[i] === 1) {
@@ -55,7 +53,7 @@ document.getElementById('replay').addEventListener('click', init, render)
         squares[i].textContent = ''
       }
     }
-
+    // renders a message about the state of the game 
     if(winner === null){
       gameStatus.textContent = `It's ${playerName()}'s turn!` 
     } else if(winner === "T") {
@@ -75,44 +73,44 @@ document.getElementById('replay').addEventListener('click', init, render)
   } 
  
 
-// HANDLE CLICK FUNCTION - when a square is clicked
+// HANDLE CLICK FUNCTION 
+  // what happens when a square is clicked
 
 function handleClick(evt){
-  // find the id of the element that was clicked 
+  // finds the id of the element that was clicked 
   let clickedSquare = evt.target.id
-  // turn its id into a number
-  // this number corresponds to an index which = a spot on the board in the board Array
+  // converts the id string into a number
+    // this number is the same as its index in boardArray, which refers to its spot on the board
   let sqIdx = parseInt(clickedSquare.split('').splice(2, 2).join(''))
-  // if the board has a value at that index (index of the element that was clicked), return
-  // if there is a winner, return
+  // returns if the board already has a value at that index
+  // returns if there is a winner
 if(boardArray[sqIdx] !== null){
   return
 }  else if (winner !== null){
   return 
 } else {
+// updates boardArray at the current index with the value of turn , so either an X or O will be displayed on the square that was clicked, depending on whose turn it is
   boardArray[sqIdx] = turn
 }
-// update board array at the index, with the value of turn
-  
-// change turn 
+// changes turn 
 turn = turn * -1
-// console.log(boardArray)
+// updates the state of the game
 render()
+// checks if there is a winner 
 getWinner()
-
 }
 
 function getWinner(){
-  // loop through each of the winning combos- each array in the winCombos array
+  // loops through winCombos array
  winCombos.forEach(function(combo){
-  //  console.log(combo)
    const a = combo[0]
    const b = combo[1]
    const c = combo[2]
-  //  console.log(a)
+  // checks if three squares that make up a winning combination are occupied
    if(Math.abs(boardArray[a] + boardArray[b] + boardArray[c]) === 3){
+     // sets the winner to either X or O, depending on the value of the first element in the current winCombos array  
      winner = boardArray[a]
-     console.log(winner)
+    // checks if every square on the board is occupied and if so, sets winner to tie
    } else if (boardArray.every(function(element){
      return element !== null
    })){
@@ -120,7 +118,7 @@ function getWinner(){
    } else {
      return null
    }
-   // sets winnerName to display in render message
+   // sets winner's name to display in the render message
    if(winner === 1){
      winnerName = 'X'
    }
